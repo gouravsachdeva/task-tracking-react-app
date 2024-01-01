5089340642ba573bda526034400cd15c62e3aa9c
gouravsachdeva <gouravsachdeva95@gmail.com>
2024-01-01 23:32:43 +0530

    Fixing Cannot find droppable entry with id in Strict mode

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,6 +3,7 @@ import "./styles.css";
 import { Todo } from "../model";
 import SingleTodo from "./SingleTodo";
 import { Droppable } from "react-beautiful-dnd";
+import { useStrictDroppable } from "../utils/useStrictDroppable";
 
 interface Props {
   todos: Array<Todo>;
@@ -17,53 +18,58 @@ const TodoList: React.FC<Props> = ({
   completedTodos,
   setCompletedTodos,
 }) => {
+  const [enabled] = useStrictDroppable(false);
   return (
     <div className="container">
-      <Droppable droppableId="TodosList">
-        {(provided, snapshot) => (
-          <div
-            className={`todos ${snapshot.isDraggingOver ? "dragactive" : ""}`}
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            <span className="todos__heading">Active Tasks</span>
-            {todos.map((todo, index) => (
-              <SingleTodo
-                index={index}
-                todo={todo}
-                key={todo.id}
-                todos={todos}
-                setTodos={setTodos}
-              />
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
+      {enabled && (
+        <Droppable droppableId="TodosList">
+          {(provided, snapshot) => (
+            <div
+              className={`todos ${snapshot.isDraggingOver ? "dragactive" : ""}`}
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              <span className="todos__heading">Active Tasks</span>
+              {todos.map((todo, index) => (
+                <SingleTodo
+                  index={index}
+                  todo={todo}
+                  key={todo.id}
+                  todos={todos}
+                  setTodos={setTodos}
+                />
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      )}
 
-      <Droppable droppableId="TodosRemove">
-        {(provided, snapshot) => (
-          <div
-            className={`todos remove ${
-              snapshot.isDraggingOver ? "dragcomplete" : ""
-            }`}
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            <span className="todos__heading">Completed Tasks</span>
-            {completedTodos.map((todo, index) => (
-              <SingleTodo
-                index={index}
-                todo={todo}
-                key={todo.id}
-                todos={completedTodos}
-                setTodos={setCompletedTodos}
-              />
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
+      {enabled && (
+        <Droppable droppableId="TodosRemove">
+          {(provided, snapshot) => (
+            <div
+              className={`todos remove ${
+                snapshot.isDraggingOver ? "dragcomplete" : ""
+              }`}
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              <span className="todos__heading">Completed Tasks</span>
+              {completedTodos.map((todo, index) => (
+                <SingleTodo
+                  index={index}
+                  todo={todo}
+                  key={todo.id}
+                  todos={completedTodos}
+                  setTodos={setCompletedTodos}
+                />
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      )}
     </div>
   );
 };
